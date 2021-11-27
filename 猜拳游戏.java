1325779241b03b7689930fcef1235d7d5f81ce0f
LIYIKANG <45025866+LIYIKANG@users.noreply.github.com>
2021-11-27 23:00:07 +0900

    Update 猜拳游戏.java

--- 猜拳游戏.java
+++ 猜拳游戏.java
@@ -7,51 +7,53 @@ class test
 	{
 		 while (true) {
             System.out.println("*******************************");
-            System.out.println("--------欢迎进入猜拳游戏--------");
-            System.out.println("请出拳:（1是剪刀，2是石头，3是布）");
+            System.out.println("--------じゃんけんゲーム--------");
+            System.out.println("何を出しますか:（1はチョキ，2はグウ，3はパア）");
             Scanner sc=new Scanner(System.in);
-            int person=(int)(Math.random()*3)+1; //获取用户输入
-            int computer=(int)(Math.random()*3)+1; //电脑随机出拳
-            String per="用户";
-            String com = "电脑";
-            //用户出拳
+            int person=sc.nextInt(); //ユーザ側
+            System.out.println("入力した値："+ person);
+            int computer=(int)(Math.random()*3)+1; //コンピューター側
+            System.out.println("入力した値："+ computer);
+            String per="ユーザ";
+            String com = "コンピューター";
+            //それぞれ定義するperson = 1,2,3の場合それぞれチョキ，グウ，パアに対応する
             switch(person){
                 case 1:
-                    per="剪刀";
+                    per="チョキ";
                     break;
                 case 2:
-                    per="石头";
+                    per="グウ";
                     break;
                 case 3:
-                    per="布";
+                    per="パア";
                     break;
             }
-            //电脑出拳
+            //同上
             switch(computer){
                 case 1:
-                    com="剪刀";
+                    com="チョキ";
                     break;
                 case 2:
-                    com="石头";
+                    com="グウ";
                     break;
                 case 3:
-                    com="布";
+                    com="パア";
                     break;
             }
 
-            //根据出拳判断输赢
-            if(person==1&&computer==2||person==2&&computer==3||person==3&&computer==1){
-                System.out.println("你出的是（"+per+"） 电脑出的是（"+com+"）");
-                System.out.println("       【你输了！再来一次吧】");
+            //じゃんけんゲームを判断する
+            if(person==1&&computer==2||person==2&&computer==3||person==3&&computer==1){//負けた場合
+                System.out.println("あなたは（"+per+"） コンピューターは（"+com+"）");
+                System.out.println("       【負けました！もう一度やり直してください】");
                 //System.out.println();
-            }else if (person==computer){
-                System.out.println("你出的是（"+per+"） 电脑出的是（"+com+"）");
-                System.out.println("       【平局！再来一次吧】");
+            }else if (person==computer){//一緒になった場合
+                System.out.println("あなたは（"+per+"） コンピューターは（"+com+"）");
+                System.out.println("       【引き分けです！もう一度やり直してください】");
                 // System.out.println();
-            }else{
-                System.out.println("你出的是（"+per+"） 电脑出的是（"+com+"）");
-                System.out.println("       【恭喜你赢了！！！】");
-                System.out.println("【你终于战胜了电脑，游戏结束！】");;
+            }else{//ほかの場合、つまり、勝ったの場合
+                System.out.println("あなたは（"+per+"） コンピューターは（"+com+"）");
+                System.out.println("       【おめでとうございます！】");
+                System.out.println("【やっとコンピューターを勝ちました！おめでとうございます！】");;
                 break;
             }
         }
